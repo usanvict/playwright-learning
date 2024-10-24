9049678009103b603893a199218e49cadc025d59
Victoria Usan <victoria.usan@gmail.com>
2024-10-24 11:26:17 +0200

    Adding API tests

---
+++ api.config.ts
@@ -0,0 +1,17 @@
+import type { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+	timeout: 60000,
+	retries: 0,
+	testDir: "tests/api",
+	use: {
+		viewport: { width: 1200, height: 720 },
+		actionTimeout: 15000,
+		ignoreHTTPSErrors: true,
+		video: "off",
+		screenshot: "off",
+		baseURL: "https://www.reqres.in/",
+	},
+};
+
+export default config;
---
+++ tests/api/api.spec.ts
@@ -0,0 +1,59 @@
+import { test, expect } from "@playwright/test";
+
+test.describe
+	.parallel("API tests in parallel", () => {
+		const baseURL = "https://petstore.swagger.io/v2";
+
+		test("Simple API Test - Assert Response Status", async ({ request }) => {
+			const response = await request.get(`${baseURL}/pet/2`);
+			expect(response.status()).toBe(200);
+		});
+
+		test("Simple API Test - Assert Invalid Endpoint", async ({ request }) => {
+			const response = await request.get(`${baseURL}/non-existing-endpoint`);
+			expect(response.status()).toBe(404);
+		});
+
+		test("GET Request - Get Dog Detail", async ({ request }) => {
+			const response = await request.get(`${baseURL}/pet/2`);
+			const responseBody = JSON.parse(await response.text());
+			expect(response.status()).toBe(200);
+			expect(responseBody.id).toBe(2);
+			expect(responseBody.name).toBeTruthy();
+			expect(responseBody.status).toBeTruthy();
+		});
+
+		test("POST Request - Create new dog", async ({ request }) => {
+			const response = await request.post(`${baseURL}/pet`, {
+				data: {
+					name: "Alex",
+					category: {
+						id: 5,
+					},
+				},
+			});
+
+			const responseBody = JSON.parse(await response.text());
+			expect(responseBody.id).toBeTruthy();
+		});
+
+		test("GET Request - Login", async ({ request }) => {
+			const response = await request.get(`${baseURL}/user/login`, {
+				data: {
+					username: "admin",
+					password: "admin",
+				},
+			});
+			expect(response.status()).toBe(200);
+		});
+
+		test.skip("GET Request - Login Failed", async ({ request }) => {
+			const response = await request.get(`${baseURL}/user/login`, {
+				data: {
+					username: "admin",
+					password: "",
+				},
+			});
+			expect(response.status()).toBe(400);
+		});
+	});
